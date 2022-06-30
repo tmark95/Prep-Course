f01ffc6ca43aa4f49e6695e2367d7afd4ce3dcfc
Tomas Mark <tomasemark@gmail.com>
2022-06-30 04:09:36 -0300

    commit dowhile

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -197,7 +197,13 @@ function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
-  
+  let suma = numero + i;
+  const i = 5;
+  do{
+    suma = suma + i;
+  } while(suma < numero + 40);
+
+  return suma;
 }
 
 
