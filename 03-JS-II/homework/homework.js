945f0ad6a5983220553645d795f4a6aabb7e4508
Tomas Mark <tomasemark@gmail.com>
2022-06-30 03:54:51 -0300

    correccion operadores logicos

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -137,11 +137,14 @@ function operadoresLogicos(num1, num2, num3) {
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false.
   if(num1 > num2 && num1 > num3 && num1 > 0){
     return 'Número 1 es mayor y positivo';
-  } if (num1 < 0 || num2 < 0 || num3 < 0){
+  } 
+  if (num1 < 0 || num2 < 0 || num3 < 0){
     return 'Hay negativos';
-  } if(num3 > num2 && num3 > num1){
+  } 
+  if(num3 > num2 && num3 > num1){
     return num3++;
-  } if (num1 == 0 || num2 == 0 || num3 == 0){
+  } 
+  if (num1 == 0 || num2 == 0 || num3 == 0){
     return 'Error';
   }
   return false;
