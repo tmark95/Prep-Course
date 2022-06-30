7ddfb551f57d51373db045a154a68cf58ce14798
Tomas Mark <tomasemark@gmail.com>
2022-06-30 02:49:28 -0300

    Quinto cambio

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -201,7 +201,7 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  return "Hola" + '' + "!";
+  return "Hola" + ' ' + nombre + "!";
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
@@ -229,7 +229,7 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-  
+  return euro*1.20;
 }
 
 
@@ -239,7 +239,13 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  
+  if(letra == 'a'|| letra == 'e'|| letra == 'i'|| letra == 'o'|| letra == 'u' ){
+    return 'Es vocal';
+  } else if(letra.length > 1){
+    return 'Dato incorrecto';
+  }
+
+  return 'Dato incorrecto';
 }
 
 
