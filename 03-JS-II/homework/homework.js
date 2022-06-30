ada168c51115fa1c2a40d75ba7bd7ea12f4969e3
Tomas Mark <tomasemark@gmail.com>
2022-06-30 04:10:30 -0300

    correccion dowhile

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -197,8 +197,8 @@ function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
-  let suma = numero + i;
   const i = 5;
+  let suma = numero + i;
   do{
     suma = suma + i;
   } while(suma < numero + 40);
