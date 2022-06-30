ec2636832618e595cdc8c76a771619f26547e5ac
Tomas Mark <tomasemark@gmail.com>
2022-06-30 03:00:11 -0300

    second commit

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -5,6 +5,12 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
+  if(x>y){
+    return x;
+  } else if(x<y){
+    return y;
+  }
+  return x;
 }
 
 function mayoriaDeEdad(edad) {
